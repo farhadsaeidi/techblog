f238e59e3585f701e72c2a59a225d604e2d1d112
farhadsaeidi <frhd.sd17@gmail.com>
2025-10-10 01:39:26 +0330

    flutter gen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tech_blog/gen/assets.gen.dart';
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'package:tech_blog/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +15,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // تنطیم فونت
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'IRANSANS',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+          headlineLarge: TextStyle(
+            fontFamily: 'DANA',
+            fontSize: 17,
+            fontWeight: FontWeight.w700,
+            color: Colors.black,
+          ),
+          headlineMedium: TextStyle(
+            fontFamily: 'DANA',
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+          titleLarge: TextStyle(
+            fontFamily: 'DANA',
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+          titleMedium: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            color: Colors.green,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+            color: Colors.black87,
+          ),
+          labelSmall: TextStyle(
+            fontSize: 11,
+            fontWeight: FontWeight.w200,
+            color: Colors.grey[700],
+          ),
+          labelLarge: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            color: Colors.red,
+          ),
+        ),
       ),
+      // تنظیمات راست چین کردن پروژه
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa'), // فارسی
+      ],
+      locale: const Locale('fa'), // زبان پیش‌فرض
       home: Home(),
     );
   }
@@ -30,25 +95,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        // Using Flutter Generator
+        child: Assets.images.logo.image() 
       ),
     );
   }
---
+++ lib/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static const String viewHottestPosts = "مشاهده داغ ترین نوشته ها";
+  static const String viewHottestPodCasts = "مشاهده داغ ترین پادکست ها";
+  static const String relatedArticles = "مقاله های مرتبط";
+  static const String submitArticle = "ارسال مقاله";
+  static const String submitPodCast = "ارسال پادکست";
+
+}
